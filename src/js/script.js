92ef321c8a72e282df9d4c2f963fd8fc69e1fb44
Luiz Gustavo Gonçalves Brito <112624030+luizbrito6@users.noreply.github.com>
2023-05-21 23:42:40 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+// const publicKey = '2ca20e1feb98f89bdd482e93888d175e';
+// const privateKey = '9be219a5a78419c50c20ef81375c001710151b68';
+// const timestamp = new Date().getTime();
+// const hash = SparkMD5.hash(timestamp + privateKey + publicKey)
+
+// const url = `https://gateway.marvel.com/v1/public/events?apikey=${publicKey}&ts=${timestamp}&hash=${hash}`;
+
+// fetch(url)
+//   .then(response => response.json())    
+//   .then(data => {
+//     console.log(data);
+//     // Manipule os dados da resposta da API da Marvel aqui
+    
+//   })
+//   .catch(error => {
+//     console.error('Erro:', error);
+//   });
